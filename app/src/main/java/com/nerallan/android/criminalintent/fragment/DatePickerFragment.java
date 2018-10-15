2eed4fdd0fa16547a43209194029159d535dec59
Nerallan <budkomaximidu@gmail.com>
2018-10-15 12:40:10 +0300

    DatePicker widget added

--- app/src/main/java/com/nerallan/android/criminalintent/fragment/DatePickerFragment.java
+++ app/src/main/java/com/nerallan/android/criminalintent/fragment/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.nerallan.android.criminalintent.R;
 
@@ -20,8 +22,13 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // filling view dialog date picker
+        View view = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
                 // dialog box settings
+                .setView(view)
                 .setTitle(R.string.date_picker)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
