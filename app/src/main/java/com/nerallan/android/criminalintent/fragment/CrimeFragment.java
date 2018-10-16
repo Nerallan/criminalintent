477e02e44f31b8f2c165df83a461879e4ed4cee7
Nerallan <budkomaximidu@gmail.com>
2018-10-16 11:04:40 +0300

    transfer data between two fragments, which host in one activity - CrimeFragmant, DatePickerFragment(update date in Crime model)

--- app/src/main/java/com/nerallan/android/criminalintent/fragment/CrimeFragment.java
+++ app/src/main/java/com/nerallan/android/criminalintent/fragment/CrimeFragment.java
@@ -1,6 +1,8 @@
 package com.nerallan.android.criminalintent.fragment;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -38,6 +40,7 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
 
     private EditText mTitleField;
     private Button mDateButton;
@@ -83,12 +86,18 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
 
-        mDateButton.setText(formatDate(mCrime.getDate()));
+        updateDate();
         mDateButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                // CrimeFragment is assigned as a target fragment for DatePickerFragment
+                // Using the request code, the target fragment can later determine
+                // which fragment returns information.
+                // FragmentManager saves the target fragment and the request code
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                // The string parameter uniquely identifies the DialogFragment in the FragmentManager list.
                 dialog.show(manager, DIALOG_DATE);
             }
         });
@@ -105,6 +114,18 @@ public class CrimeFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
 
     // This method creates an instance of the fragment, packages it, and sets its arguments to the fragment
     // to fragment was not tied to a specific host activity
@@ -118,6 +139,12 @@ public class CrimeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+
+    private void updateDate() {
+        mDateButton.setText(formatDate(mCrime.getDate()));
+    }
+
 
     private String formatDate(Date pDate){
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMM d, y", Locale.getDefault());
--- app/src/main/java/com/nerallan/android/criminalintent/fragment/DatePickerFragment.java
+++ app/src/main/java/com/nerallan/android/criminalintent/fragment/DatePickerFragment.java
@@ -1,14 +1,22 @@
 package com.nerallan.android.criminalintent.fragment;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.nerallan.android.criminalintent.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static android.support.v4.os.LocaleListCompat.create;
 
@@ -18,19 +26,69 @@ import static android.support.v4.os.LocaleListCompat.create;
 
 public class DatePickerFragment extends DialogFragment {
 
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static final String EXTRA_DATE = "com.nerallan.android.criminalintent.date";
+
     // FragmentManager instance of host-activity calls this method during DialogFragment output to the screen
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // get date from DatePickerFragment args package
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        // to initialize the datepicker, you must have integer values for month, day, and year.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         // filling view dialog date picker
         View view = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) view.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 // dialog box settings
                 .setView(view)
                 .setTitle(R.string.date_picker)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the application should get the date from the datepicker and
+                        // send the result to the CrimeFragment ( calls sendResult() )
+                        int year = mDatePicker.getYear();
+                        int month = mDatePicker.getMonth();
+                        int day = mDatePicker.getDayOfMonth();
+                        Date date = new GregorianCalendar(year, month, day).getTime();
+                        sendResult(Activity.RESULT_OK, date);
+                    }
+                })
                 .create();
+    }
+
+
+    public static DatePickerFragment newInstance(Date pDate){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, pDate);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    // creates an intent, puts the date in it as an addition, and then calls the CrimeFragment.onActivityResult (...)
+    private void sendResult(int pResultCode, Date pDate){
+        if (getTargetFragment() == null){
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, pDate);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), pResultCode, intent);
     }
 }
